6c26044ca839ffe8c67b1e3cf32a9839f720665c
Ha Phan <ha.markus.phan@gmail.com>
2023-11-30 23:29:47 +0000

    successful put the marker on the location search

--- assets/script/location.js
+++ assets/script/location.js
@@ -22,54 +22,54 @@
 
 // })
 
-async function getRecipe(){
-    const url = 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=brownie';
-    const options = {
-	    method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': 'e0faed762bmsh60b9c6ae7e9fa90p165d20jsn437c35637387',
-		    'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
-	    }
-    };
+// async function getRecipe(){
+//     const url = 'https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=brownie';
+//     const options = {
+// 	    method: 'GET',
+// 	    headers: {
+// 		    'X-RapidAPI-Key': 'e0faed762bmsh60b9c6ae7e9fa90p165d20jsn437c35637387',
+// 		    'X-RapidAPI-Host': 'recipe-by-api-ninjas.p.rapidapi.com'
+// 	    }
+//     };
 
-    try {
-	    const response = await fetch(url, options);
-	    const result = await response.text();
-	    console.log(result);
-    } catch (error) {
-	    console.error(error);
-    }
-}
-getRecipe();
+//     try {
+// 	    const response = await fetch(url, options);
+// 	    const result = await response.text();
+// 	    console.log(result);
+//     } catch (error) {
+// 	    console.error(error);
+//     }
+// }
+// getRecipe();
 
-async function getRecipe1(){
-    const url = 'https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup';
-    const options = {
-	    method: 'GET',
-	    headers: {
-		    'X-RapidAPI-Key': 'e0faed762bmsh60b9c6ae7e9fa90p165d20jsn437c35637387',
-		    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-	    }
-    };
+// async function getRecipe1(){
+//     const url = 'https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup';
+//     const options = {
+// 	    method: 'GET',
+// 	    headers: {
+// 		    'X-RapidAPI-Key': 'e0faed762bmsh60b9c6ae7e9fa90p165d20jsn437c35637387',
+// 		    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+// 	    }
+//     };
 
-    try {
-	    const response = await fetch(url, options);
-	    const result = await response.text();
-	    console.log(result);
-    } catch (error) {
-	    console.error(error);
-    }
-}
-getRecipe1(); // Only ingredients
+//     try {
+// 	    const response = await fetch(url, options);
+// 	    const result = await response.text();
+// 	    console.log(result);
+//     } catch (error) {
+// 	    console.error(error);
+//     }
+// }
+// getRecipe1(); // Only ingredients
 
 var googleMapAPIKey = "AIzaSyDFfKtEpR4sFVJZEPpd4hkPhuRU6wmifGE";
 
 // Initialize and add the map from Map JavaScript API https://developers.google.com/maps/documentation/javascript
 let map;
 
-async function initMap() {
+async function initMap(lat,lng,title) {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: lat, lng: lng };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -86,11 +86,11 @@ async function initMap() {
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: title,
   });
 }
 
-initMap();
+// initMap(lat,lng,title);
 
 // When the user enters the name of a dish, the restaurants/ bakeries with that dish will appear on the browser
 // Will the user need to enter a "location" into the input? - Perhaps try this option first, or the browser will get the approximate device location based on cell towers and WiFi nodes - Geolocation
@@ -120,7 +120,12 @@ locationBtnEl.addEventListener('click',function(e){
     return response.json();
   }).then(function(data){
     console.log(data);
-    
+    console.log(data.results[0].geometry.location.lat);
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    var title = data.results[0].formatted_address;
+
+    initMap(lat,lng,title);
   })
 
 })
