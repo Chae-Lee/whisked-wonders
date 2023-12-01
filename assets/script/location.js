039e50ec64e3cccbee02a994e3e341a04bd3b12d
Ha Phan <ha.markus.phan@gmail.com>
2023-12-01 20:50:57 +0000

    test Nearby API from google

--- assets/script/location.js
+++ assets/script/location.js
@@ -1,3 +1,92 @@
+var googleMapAPIKey = "AIzaSyDFfKtEpR4sFVJZEPpd4hkPhuRU6wmifGE";
+var map;
+var service;
+var infowindow;
+var locationInputEl = document.getElementById('location-search-input');
+var locationBtnEl = document.getElementById('location-search-button');
+
+// Initialize and add the map from Map JavaScript API https://developers.google.com/maps/documentation/javascript
+async function initMap(lat,lng,title) {
+  const position = { lat: lat, lng: lng };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: title,
+  });
+}
+
+initMap(55.953252,-3.188267,"Edinburgh"); //Default location, just because I like Edinburgh
+
+locationBtnEl.addEventListener('click',function(e){
+  e.preventDefault();
+
+  var locationInputText = locationInputEl.value.trim();
+  var geoCodingURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationInputText + "&key=" + googleMapAPIKey;
+
+  fetch(geoCodingURL)
+  .then(function(response){
+    return response.json();
+  }).then(function(dataGeoCode){
+    console.log(dataGeoCode);
+    // console.log(dataGeoCode.results[0].geometry.location.lat);
+    var lat = dataGeoCode.results[0].geometry.location.lat;
+    var lng = dataGeoCode.results[0].geometry.location.lng;
+    var title = dataGeoCode.results[0].formatted_address;
+
+    initMap(lat,lng,title);
+
+    var nearbySearchURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "%2C" + lng + "&radius=10000" + "&type=restaurant" + "&key=" + googleMapAPIKey;
+    console.log(nearbySearchURL);
+    fetch(nearbySearchURL)
+    .then(function(response1){
+      return response1.json();
+    }).then(function(dataNearby){
+      console.log(dataNearby);
+    })
+
+  }).catch(function(error){
+    console.error(error);
+  })
+
+  
+})
+
+
+
+// Test new API Text Search
+// var locationInputText = locationInputEl.value.trim();
+
+// fetch('https://places.googleapis.com/v1/places:searchText',{
+//   method: "POST",
+//   headers:{
+//     "textQuery": locationInputText,
+//     "Content-Type": "application/json",
+//     "X-Goog-API-Key": googleMapAPIKey,
+//     "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.priceLevel"
+//   },
+//   body: JSON.stringify({})
+// })
+//   .then(function(response){
+//     return response.JSON();
+//   }).then(function(data){
+//     console.log(data);
+//   }).catch(function(error){
+//     console.error(error);
+//   })
+
+
 // var rapidURL = "https://api.api-ninjas.com/v1/recipe?query=";
 // var query = "tiramisu"; // Query text to search
 // var XApiKey = "e0faed762bmsh60b9c6ae7e9fa90p165d20jsn437c35637387";
@@ -62,70 +151,42 @@
 // }
 // getRecipe1(); // Only ingredients
 
-var googleMapAPIKey = "AIzaSyDFfKtEpR4sFVJZEPpd4hkPhuRU6wmifGE";
 
-// Initialize and add the map from Map JavaScript API https://developers.google.com/maps/documentation/javascript
-let map;
 
-async function initMap(lat,lng,title) {
-  // The location of Uluru
-  const position = { lat: lat, lng: lng };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 15,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: title,
-  });
-}
 
-initMap(55.953252,-3.188267,"Edinburgh"); //Default location, just because I like Edinburgh
+
 
 // When the user enters the name of a dish, the restaurants/ bakeries with that dish will appear on the browser
 // Will the user need to enter a "location" into the input? - Perhaps try this option first, or the browser will get the approximate device location based on cell towers and WiFi nodes - Geolocation
 // Geolocation seems to be a bit more complicated, let's try that later
 
-var userAddress = "24%20Sussex%20Drive%20Ottawa%20ON";
-var geoCodingURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userAddress + "&key=" + googleMapAPIKey;
+// var userAddress = "24%20Sussex%20Drive%20Ottawa%20ON";
+// var geoCodingURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userAddress + "&key=" + googleMapAPIKey;
 
-fetch(geoCodingURL)
-.then(function(response){
-  return response.json();
-}).then(function(data){
-  console.log(data);
-}) // Pass the lat and long values from the geoCoding into the Marker function
+// fetch(geoCodingURL)
+// .then(function(response){
+//   return response.json();
+// }).then(function(data){
+//   console.log(data);
+// }) // Pass the lat and long values from the geoCoding into the Marker function
 
-var locationInputEl = document.getElementById('location-search-input');
-var locationBtnEl = document.getElementById('location-search-button');
 
-locationBtnEl.addEventListener('click',function(e){
-  e.preventDefault();
 
-  var locationInputText = locationInputEl.value.trim();
-  var geoCodingURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationInputText + "&key=" + googleMapAPIKey;
+// function nearbySearch(lat,lng) {
+//   var nearbySearchURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters" + "&location=" + lat + lng;
 
-  fetch(geoCodingURL)
-  .then(function(response){
-    return response.json();
-  }).then(function(data){
-    console.log(data);
-    console.log(data.results[0].geometry.location.lat);
-    var lat = data.results[0].geometry.location.lat;
-    var lng = data.results[0].geometry.location.lng;
-    var title = data.results[0].formatted_address;
+// }
 
-    initMap(lat,lng,title);
-  })
+// https://developers.google.com/maps/documentation/places/web-service/search-nearby
 
-})
+// eample: https://maps.googleapis.com/maps/api/place/nearbysearch/json
+// ?keyword=cruise
+// &location=-33.8670522%2C151.1957362
+// &radius=1500
+// &type=restaurant
+// &key=YOUR_API_KEYx
+
+// service = new google.maps.places.PlacesService(map);
+// service.nearbySearch(request,callback);
