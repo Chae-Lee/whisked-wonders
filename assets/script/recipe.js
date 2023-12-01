7ef1c7733bd6f1561c7909ddd970c0e356ee132c
Chae-Lee <katelee1129@gmail.com>
2023-12-01 23:55:15 +0000

    addition of fetch call

--- assets/script/recipe.js
+++ assets/script/recipe.js
@@ -1,23 +1,15 @@
 /* Pseudocode
-  1) When the button 'easy', 'medium', 'hard' is clicked then recipe under each categories are pulled and displayed on the page as a list 
-    - addEventListner for the buttons 
+  1) fetch all the data from the birthday cake API
+  2) write a for loop to iterate through the recipe and distribute into 3 arrays 'easy', 'medium' and 'hard'
+    - global array 
+  3) When the button 'easy', 'medium', 'hard' is clicked then recipe under each categories are pulled and displayed on the page as a list 
+    - addEventListener for the buttons 
     - API return data : iterate through the recipes and display matching the selected difficulty 
     - for loop so that this occurs for all levels of difficulty 
-  2) When a recipe is selected the full recipe with the ingredients, methods and image displays 
+  4) When a recipe is selected the full recipe with the ingredients, methods and image displays 
     - on a separate page? or directly within the recipe html page
 */
-// Testing out public APIs for recipes and drinks
-var recipeAPI = 'https://the-birthday-cake-db.p.rapidapi.com/';
-var apiKey = '637d11b810msh7d4930cc5d05c6ep1b0fcdjsnb524ebfaaff6';
-
-fetch (recipeAPI)
-  .then (function (response){
-    return response.jason ();
-  }) .then(function (data){
-    console.log (data);
-  })
-
-// Confirmation that the API data can be fetched
+// // Testing and Confirmation that the API data can be fetched
 var fetchData = async ()=>{
 const url = 'https://the-birthday-cake-db.p.rapidapi.com/';
 const options = {
@@ -27,14 +19,30 @@ const options = {
 		'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.com'
 	}
 };
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}}
+// //try catch statement that can be used to pull the data from the API
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }}
+
+fetch (url, options)
+  .then (function(response){
+  return response.json ();
+}).then (function(data){
+  console.log (data);
+})
+};
 
 fetchData ();
 
+//Variables 
+var easy =[];
+var medium = [];
+var hard = [];
 
+for (var i=0; i<fetchData.length; i++){
+  console.log (i);
+}
